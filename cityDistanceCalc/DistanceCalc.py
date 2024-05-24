de7ed35d0682519e8fd347c205f8b9e362d3450a
andrewMorrison20 <158091930+andrewMorrison20@users.noreply.github.com>
2024-05-24 18:50:54 +0100

    firstCommit

--- cityDistanceCalc/DistanceCalc.py
+++ cityDistanceCalc/DistanceCalc.py
@@ -1,1 +1,10 @@
-//City Distance Calculator
+'''
+Calculate the distance between you Cities
+'''
+from pygeocoder import Geocoder
+import sys
+import numpy as np
+
+def get_distance(pointA,pointB):
+    planet_radius = 6371
+    lat = np.
